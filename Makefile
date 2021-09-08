813478db0f8c356b14cc3ba551f91b495d3d40f9
blais <blais@furius.ca>
2021-09-07 21:42:29 -0400

    Automated the update of Python mappings for long-term maintenance and up-to-date mulmat.

--- Makefile
+++ Makefile
@@ -1,13 +1,23 @@
 #!/usr/bin/env make
+#
+# Simple makefile I use for long-term maintenance and update of the CME mappings.
+#
 
 OLD=$(PWD)/data/cme-expirations.csv
 NEW=$(PWD)/data/cme-expirations-new.csv
 
-update update-db:
+# Update the local CSV database to a temporary file.
+update-db:
 	mulmat-update-db $(OLD) $(NEW)
 
+# Visually inspect the differences/updates.
 diff:
 	xxdiff $(OLD) $(NEW)
 
+# Commit the temporary file to the main database.
 clobber:
 	mv $(NEW) $(OLD)
+
+# Regenerate Python mappings from the CSV database.
+update-py:
+	mulmat-update-py --database=$(OLD)
--- experiments/enumerate-products.py
+++ experiments/enumerate-products.py
@@ -15,87 +15,7 @@ import petl
 from petl import Table
 
 import mulmat
-
-
-def split_month(symbol: str) -> Tuple[str, str, str]:
-    match = re.match('(.*)([FGHJKMNQUVXZ])(\d{1,2})$', symbol)
-    if match is None:
-        raise ValueError
-    return match.groups()
-
-
-def list_products(options: Table):
-    """Print a mapping of (option, underlying) product codes."""
-
-    # Calculate a mapping of option product code to corresponding underlying.
-    mapping = collections.defaultdict(set)
-    for rec in options.records():
-        try:
-            option_product, _, __ = split_month(rec['Symbol'])
-            under_product, _, __ = split_month(rec['Underlying'])
-        except ValueError:
-            continue
-        mapping[option_product].add(under_product)
-
-    # Check uniqueness.
-    for option, underlyings in list(mapping.items()):
-        if len(underlyings) > 1:
-            raise ValueError(
-                f"Multivalent mapping from options to futures: {option} {underlying}")
-        mapping[option] = next(iter(underlyings))
-
-    pprint.pprint(dict(mapping))
-
-
-def only(aset: set[Any]) -> Any:
-    assert len(aset) == 1
-    return next(iter(aset))
-
-
-def list_months(options: Table):
-    """Print mapping of ((option, option-month), (underlying, underlying-month)) codes."""
-
-    # Calculate a mapping of option product code to corresponding underlying.
-    mapping = collections.defaultdict(lambda: collections.defaultdict(lambda: collections.defaultdict(set)))
-    for rec in options.records():
-        try:
-            oproduct, omonth, oyear = split_month(rec['Symbol'])
-            uproduct, umonth, uyear = split_month(rec['Underlying'])
-        except ValueError:
-            continue
-        yeardiff = int(uyear)-int(oyear)
-        if yeardiff >= 10:
-            continue
-        mapping[uproduct][oproduct][omonth].add((umonth, yeardiff))
-
-    # Check uniqueness.
-    for item in mapping.items():
-        uproduct, oproducts = item
-        for oproduct, omonths in oproducts.items():
-            for omonth, umonths in omonths.items():
-                if len(umonths) > 1:
-                    raise ValueError(
-                        f"Multivalent mapping from options to futures: {item}")
-                umonth, yeardiff = only(umonths)
-                if yeardiff != 0:
-                    pass # print(uproduct, oproduct, omonth, umonth, yeardiff)
-
-    # Remove default dicts.
-    mapping = {key1: {key2: {key3: only(value3)
-                             for key3, value3 in value2.items()}
-                      for key2, value2 in value1.items()}
-               for key1, value1 in mapping.items()}
-    ##pprint.pprint(mapping)
-
-    # Reconstruct a mapping for option to underlying, with the year difference.
-    option_under_mapping = {}
-    for uproduct, oproducts in mapping.items():
-        for oproduct, omonths in oproducts.items():
-            for omonth, (umonth, yeardiff) in omonths.items():
-                option_under_mapping[(oproduct, omonth)] = (uproduct, umonth, yeardiff)
-
-    pprint.pprint(option_under_mapping)
-
+import mulmat.lists
 
 
 @click.command()
@@ -110,9 +30,9 @@ def main(database: Optional[str], products: bool, months: bool):
                .cut('Symbol', 'Underlying'))
 
     if products:
-        list_products(options)
+        pprint.pprint(mulmat.lists.list_products(options))
     if months:
-        list_months(options)
+        pprint.pprint(mulmat.lists.list_months(options))
 
 
 if __name__ == '__main__':
