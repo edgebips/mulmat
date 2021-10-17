2b808528671fecfc78907b5de989f2ec8b366ba6
blais <blais@furius.ca>
2021-10-17 15:57:12 -0400

    Added settlement mappings.

---
+++ mulmat/settlement.py
@@ -0,0 +1,12 @@
+"""Settlement symbols for indices.
+"""
+
+__copyright__ = "Copyright (C) 2021  Martin Blais"
+__license__ = "GNU GPLv2"
+
+SETTLEMENT = {
+    'SPX': 'SET',
+    'RUT': 'RLS',
+    'NDX': 'XQ0',
+}
+# TODO(blais): Add more mappings to settlement values.
